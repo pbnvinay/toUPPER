95975aab2a5cc3a43bab6571885e12249e090154
pbnvinay <vinay.pbn@gmail.com>
2024-05-07 14:48:34 +0530

    Add files via upload

---
+++ toUppercase/src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Main {
+
+    public static void main(String[] args) {
+        toUppercase obj=new toUppercase("vinay".toCharArray());
+    }
+    }
+
---
+++ toUppercase/src/main/java/org/example/toUppercase.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class toUppercase {
+    public toUppercase(char[] str) {
+        for (int i = 0, e = str.length; i < e; ++i) {
+            if (Character.isLowerCase(str[i])) {
+                str[i] = (char) (str[i] - 32);
+            }
+        }
+        System.out.println(new String(str));
+    }
+}
