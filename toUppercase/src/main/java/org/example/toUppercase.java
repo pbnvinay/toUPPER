ec0ee881cabaafcfbc9ee1f1f20ada4db4cb9c47
pbnvinay <vinay.pbn@gmail.com>
2024-05-07 15:07:31 +0530

    toUppercase.java

--- toUppercase/src/main/java/org/example/toUppercase.java
+++ toUppercase/src/main/java/org/example/toUppercase.java
@@ -1,9 +1,9 @@
 package org.example;
 
 public class toUppercase {
-    public toUppercase(char[] str) {
+    public void toUpper(char[] str) {
         for (int i = 0, e = str.length; i < e; ++i) {
-            if (Character.isLetter(str[i]) && Character.isLowerCase(str[i])) {
+            if (Character.isLowerCase(str[i]) && Character.isLetter(str[i])) {
                 str[i] = (char) (str[i] - 32);
             }
         }
