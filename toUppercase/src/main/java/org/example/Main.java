619da14054ae450ce3588e89fe719f627e21d830
pbnvinay <vinay.pbn@gmail.com>
2024-05-07 15:36:35 +0530

    Main.java

--- toUppercase/src/main/java/org/example/Main.java
+++ toUppercase/src/main/java/org/example/Main.java
@@ -1,10 +1,8 @@
 package org.example;
 
 public class Main {
-
     public static void main(String[] args) {
-        toUppercase obj=new toUppercase();
-        obj.toUpper("vinay".toCharArray());
+        Uppercase obj=new Uppercase();
+        obj.toUpper("vinay");
     }
-    }
-
+}
