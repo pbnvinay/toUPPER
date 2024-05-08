35d71c3e5251f432ff03ac6a6416eea1902019d5
pbnvinay <vinay.pbn@gmail.com>
2024-05-08 14:17:07 +0530

    Main.java

--- toUppercase/src/main/java/org/example/Main.java
+++ toUppercase/src/main/java/org/example/Main.java
@@ -3,6 +3,12 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         Uppercase obj=new Uppercase();
-        obj.toUpper("vinay");
+        System.out.println(obj.TCnormal());
+        // not accepting nullvalues as a input so if you want to normal flow you comment TCnegative
+        //System.out.println(obj.TCnegative());
+        System.out.println(obj.TCempty());
+        System.out.println(obj.TCnumber());
+        System.out.println(obj.TCmixed());
+
     }
 }
